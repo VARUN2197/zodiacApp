35bf0e3fbe71665080e9855835e382ffa9f3b116
vgusain <vgusain@VGUSAIN-IN.al.oracle.com>
2019-10-23 02:42:22 +0530

    changes in images and zodiac array

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,122 +1,75 @@
 var zodiacs = [
     {
-        name : "Aries",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Aries.svg",
-        unicode_symbol : "♈",
-        gloss : "The Ram",
-        element : "Fire",
-        ruling_body_classic : "Mars",
-        ruling_body_modern: "Mars",
-        "keywords": ["driven", "idealistic", "aggressive"]
+        name: "Aries",
+        image: "/images/aries.jpg",
+        month: "March 21-April 20",
+        description: "Aries is ruled by Mars, the fiery red planet of energy and drive. In mythology, Mars was the god of war and aggression. Highly impatient and competitive, many Aries have the fighting spirit of your mythological ruler. You love to be a hero–or to be swept away by one. As the zodiac’s first sign, you were born to be number one. You’re a solo star who steals the spotlight and inspires everyone with your confidence. Yeah, you can be impatient, even a little bossy, especially when you don’t get your way. As the “baby” of the zodiac, you need lots of attention, and can throw quite the tantrum when you don’t get it. Fortunately, you rarely have a problem turning heads. Your friends love to follow as you lead them on the latest adventure. Make sure to let other people be the boss every now and then, too, or you could alienate potential allies. Focus your competitive streak into a diva-worthy goal, delegate to your troops, and you’ll rise to the top!"
     },
     {
-        name : "Taurus",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Taurus.svg",
-        unicode_symbol: "♉",
-        gloss: "The Bull",
-        element: "Earth",
-        ruling_body_classic: "Venus",
-        ruling_body_modern: "Earth",
-        keywords: ["resolute", "sensual", "indulgent"]
+        name: "Taurus",
+        image: "/images/taurus.jpg",
+        month: "April 20 to May 20",
+        description: "Sensual Taurus is ruled by Venus, the planet of love and beauty. Your sign loves great food, romance and beautiful things. Venus kicks your five senses into high gear, so you are are happiest when surrounded by the best of everything. Satin sheets, gourmet dinners, a glass of red wine, and boom…you’re relaxing into one of your famous, 14-hour naps. Not that you don’t work hard, too. Like a bull, Taurus operates at two speeds: either totally relaxed, or charging headfirst towards a target. You prefer to keep the peace, but when you’re pushed past your limit, watch out! Underneath it all, you just want to be comfortable, and surrounded by life’s finest offerings. Taurus knows what s/he likes, and that’s that. You stick with your opinions, and you don’t change unless there’s a really good reason. You can be stubborn…but on the upside, you’re also loyal and dependable. Just be careful not to get stuck in a safety zone or your life could become stagnant. Take a chance and try something new. Sometimes, change and risk can bring the progress and reward you’re wishing for."
     },
     {
         name: "Gemini",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Gemini.svg",
-        unicode_symbol: "♊",
-        gloss: "The Twins",
-        element: "Air",
-        ruling_body_classic: "Mercury",
-        ruling_body_modern: "Mercury",
-        keywords: ["playful", "curious", "two-faced"]
+        image: "/images/gemini.jpg",
+        month: "May 21-June 20",
+        description: "The Gemini mind (and mouth!) is a busy machine, always moving at warp speed. That’s because your sign is ruled by Mercury, the planet of communication. Mercury also rules technology and the mind, making Gemini one of the most curious and cutting-edge signs of the zodiac. If two is double the fun, then as the sign of the twins, you know it’s true! There are at least two personalities inside of you at all times. Adventurous Geminis change faster than the weather, and constantly flip between moods and interests. One day, you’re into frilly, girl stuff. The next day, you’re over that and skydiving out of an airplane. If possible, try to stick with a couple key passions for the long haul, and get really good at them. You love to chatter and you have a million great ideas. Keep your fast-moving mind and hands busy with email, instant messages, mobile gadgets, and great conversations. Watch a temptation to gossip. Keep a notebook handy to jot down your thoughts at all times."
     },
     {
         name: "Cancer",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Cancer.svg",
-        unicode_symbol: "♋",
-        gloss: "The Crab",
-        element: "Water",
-        ruling_body_classic: "Moon",
-        ruling_body_modern: "Moon",
-        keywords: ["nurturing", "snobby", "petty"]
+        image: "/images/cancer.jpg",
+        month: "June 21 to July 22",
+        description: "Sensitive Cancer Zodiac is ruled by the ever-changing Moon, and your moods go through many cycles. You’re intuitive and nurturing, even psychic. Pay attention to your dreams, especially near a Full Moon. You are sensitive and loyal, with a ready shoulder to cry on. Cancer loves to get domestic, serving your friends and family home-baked goodies and tea. When you get moody, you need to be comforted, or else just left alone. Like a crab, you can cling — or pinch — when you feel threatened or vulnerable. And thrifty Cancers can pinch pennies just as tightly. (Of course, you also love to spend! It’s a mood thing.) To make life easier, surround yourself with comfort: a cozy sweater, a favorite book, trusted friends, and a stash of your favorite goodies. Cancers are close to their moms, so keep yours on speed-dial and call her when you’re down. You may be an unofficial mom to everyone in your life, too. Taking care of kids, friends, and pets can keep your spirits high."
     },
     {
         name: "Leo",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Leo.svg",
-        unicode_symbol: "♌",
-        gloss: "The Lion",
-        element: "Fire",
-        ruling_body_classic: "Sun",
-        ruling_body_modern: "Sun",
-        keywords: ["fierce", "opulent", "narcissistic"]
+        image: "/images/leo.jpg",
+        month: "July 23 to August 22",
+        description: "the Sun, Leo heats everything up with its dazzling drama. And when the Sun sets, it’s ice, ice baby. That’s why friends of Leo know to keep them happy and shining…or else. Leo is the sign of drama, and oh, can you put on a show! Even the Leos who pretend to be shy come to life in the spotlight. A natural-born leader, you love to be the boss. Leos need mega-doses of praise and appreciation, which may cause people to think you’re self-centered. The truth is, you have a huge heart, and you’re the zodiac’s most generous sign. You’re always there to help, spoil, and protect the people you love. So what if your appetite is huge? Your energy may be hard for others to match. Remember, people express love in different ways, and not everyone can show it like you do. But it’s never boring to be part of your glamorous world, so let your star shine. Somebody’s gotta rule, and it might as well be you!"
     },
     {
         name: "Virgo",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Virgo.svg",
-        unicode_symbol: "♍",
-        gloss: "The Maiden",
-        element: "Earth",
-        ruling_body_classic: "Mercury",
-        ruling_body_modern: "Ceres",
-        keywords: ["meticulous", "helpful", "naive"]
+        image: "/images/virgo.jpg",
+        month: "August 23 to September 22",
+        description: "Detail-oriented Virgo is ruled by Mercury, planet of the mind and communication. Virgos are always analyzing everything, forming opinions and judgments. On the outside, you seem sweet and innocent, but your quick mind never misses a detail. Virgo is also the sign of service, and you’re always there when help is needed. You’re an amazing, careful listener, and as a result, you give the best advice. You love to analyze everything! In fact, you can drive yourself crazy, because you never stop thinking. Virgo is a perfectionist who sees every flaw, and at times, you’re a little too critical. Relax your mind by channeling it into a project, like solving a crossword puzzle, writing a story, or organizing your desk. As an earth sign, you love the outdoors, so chill out in nature, or try a sport like karate. Remember, it’s easy for helpful Virgo to give, but harder for you to receive. Let your friends pitch in and support you, too! Try opening up a little more. You don’t have to be perfect to be loveable. Just be yourself."
     },
     {
         name: "Libra",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Libra.svg",
-        unicode_symbol: "♎",
-        gloss: "The Scales",
-        element: "Air",
-        ruling_body_classic: "Venus",
-        ruling_body_modern: "Venus",
-        keywords: ["balanced", "chatty", "indecisive"]
+        image: "/images/libra.jpg",
+        month: "September 23 to October 22",
+        description: "Gentle Libra is ruled by Venus, planet of beauty and love. Perhaps that explains your smooth talk and pretty face. Like Taurus, which is also Venus-ruled, Libras love to be surrounded by art, culture and beauty in a soft, harmonious environment. You love good food and expensive things. Lovely Libra knows how to charm but behind your dimples and sweet personality, you’re a fighter. Libra is the sign of the scales, and you can’t stand anything that’s unfair or unbalanced. You love a designer outfit as much as you adore a good debate. You’ll stand up for justice, and you’ll do it with style. Libra hates to be rushed–you prefer that everything happen your way, on your time. If that means you’re ten hours late, so be it. You’re a social butterfly who loves to be surrounded by people in a beautiful setting, talking and hanging out for hours. Just remember to save some time for yourself. Libras can get so caught up in the moment, they forget to handle their own responsibilities. Learn the beauty of being organized, and you’ll have all the time in the world to enjoy your beautiful life."
     },
     {
         name: "Scorpio",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Scorpio.svg",
-        unicode_symbol: "♏",
-        gloss: "The Scorpion",
-        element: "Water",
-        ruling_body_classic: "Mars",
-        ruling_body_modern: "Pluto",
-        keywords: ["deep", "magnetic", "destructive"]
+        image: "/images/scorpio.jpg",
+        month: "October 23 to November 22",
+        description: "Intense Scorpio is ruled by Pluto, planet of power and control. Pluto is a tiny powerhouse, and a bit of a mystery; in mythology Pluto was the god of the underworld. Many Scorpios are also drawn to “dark” things, and often wear the color black, or a shocking tone like red or hot pink. Like Pluto, Scorpio’s power often emanates from a hidden source, even when you don’t say a word. Mysterious Scorpio is the zodiac’s most misunderstood sign. You’re so powerful that people feel your presence, even before you’ve said a word! Sensitive Scorpio picks up vibes. You see every little detail, and you can read people like an open book. Once your friends get used to your high-intensity style, they know that you’re fiercely loyal. Anyone who betrays you had better watch out! Like a Scorpion, you’ll deliver a painful sting of revenge. Since your sign is naturally secretive, work on being more open with people. This will develop trust, and improve your relationships. Scorpio’s concentration powers are amazing, but be careful not to get obsessed. Curb any jealous or possessive feelings by pouring your energy into a creative project or passion."
     },
     {
         name: "Sagittarius",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Sagittarius.svg",
-        unicode_symbol: "♐",
-        gloss: "The Archer",
-        element: "Fire",
-        ruling_body_classic: "Jupiter",
-        ruling_body_modern: "Jupiter",
-        keywords: ["ambitious", "daring", "coarse"]
+        image: "/images/sagittarius.jpg",
+        month: "November 22 to December 21",
+        description: "Cheerful Sagittarius is ruled by Jupiter, the largest planet in the solar system. Jupiter is the “happy” planet, full of energy and confidence. Sagittarians often reflect Jupiter’s larger-than-life persona. As the zodiac’s traveler, Sagittarius is the ultimate free spirit: optimistic, open-minded and ambitious. You’re happiest in wide-open spaces with plenty of adventure and excitement. You juggle a million projects, hobbies and friends. The more, the merrier! Sagittarius is forever pursuing a super-sized goal. Even when you fail, nothing can keep you down. You’ll chalk it up as a life lesson and inspire everyone with tales of your experience. As the sign of wisdom and truth, Sagittarius loves to “tell it like it is,” especially when it makes people laugh. However, your honesty doesn’t always come in the prettiest package. You’ve been known to bruise feelings with your bluntness. Cultivate patience, and be careful not to come off as a know-it-all. Although Sag loves the thrill of a new project or friendship, you don’t always finish what you start. Work on keeping your promises and commitments."
     },
     {
-        name: "Capricon",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Capricorn.svg",
-        unicode_symbol: "♑",
-        gloss: "The Mountain Sea-goat",
-        element: "Earth",
-        ruling_body_classic: "Saturn",
-        ruling_body_modern: "Saturn",
-        keywords: ["responsible", "awkward", "rigid"]
+        name: "Capricorn",
+        image: "/images/capricorn.jpg",
+        month: "December 22 to January 19",
+        description: "Serious Capricorn is ruled by Saturn, the planet of tough lessons and wisdom. As a result, many Capricorns are “old souls” who play by the rules and follow tradition. Accustomed to regular butt-kickings from Saturn, you often see life as an uphill battle, with the ultimate reward arriving only through suffering and sacrifice. Ambitious Capricorn is symbolized by a mountain goat climbing up a rocky hill, taking one cautious step at a time. And that’s exactly how patient Caps like to do everything. You’ll skip the good stuff today if it means getting the reward tomorrow. That’s why your sign has more trophies and loyal friends than any other–you stay strong through the hard times. Hardworking Caps are devoted to their friends and families, and people count on you for support. You always keep your word. Remember to let yourself have fun, too! Life doesn’t always have to be a struggle. It’s okay to enjoy your victories, even if you didn’t work hard to win them. Lighten up and share your wacky, down-to-earth humor. When you get moody, let your family and closest friends be there for you–just as you always are for them."
     },
     {
         name: "Aquarius",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Aquarius.svg",
-        unicode_symbol: "♒",
-        gloss: "The Water-bearer",
-        element: "Air",
-        ruling_body_classic: "Saturn",
-        ruling_body_modern: "Uranus",
-        keywords: ["inventive", "zany", "erratic"]
+        image: "/images/aquarius.jpg",
+        month: "January 20-February 18",
+        description: "Avant-garde Aquarius is ruled by Uranus, the only planet in the solar system that spins on its side! Just like your celestial overlord, you’ve been known to do things your own way, moving on a path different from everyone else’s. Some call you eccentric, others appreciate your cutting-edge originality and authentic style. One of the many ways that Aquarius is a paradox? You’re highly individualistic, but also an amazing team player. Aquarius rules the zodiac’s eleventh house of teamwork, technology and friendship. You might look like the fresh-faced guy or girl next door on the outside, but inside you march to your own beat. Many Aquarians are also naturally popular, as your vibrantly social sign loves to be among your people, telling jokes and introducing thought-provoking conversation topics. People truly do make your world go round, and you can become insta-BFFs with the most random strangers. As the sign of the future, you can be a bit of an alien—a little “out there” in your approach. Not that you care about offending anyone! Aquarius rules casual connections, and you can disengage as quickly as you connect. In fact, your platonic pals may get better treatment than your romantic partners. While you can be a bit unsentimental on a one-on-one level, you can be moved to tears by the plight of animals, the environment or other social justice issues. Yes, you’re a bohemian at heart in some ways, but you also get the job done. As a tenacious fixed sign, you can be quite hardworking when you devote yourself to a goal. A competitive (and lesser-known) Type A streak can emerge when you really want something. Nothing turns you on like progress, especially in the name of your grander ideals!"
     },
     {
         name: "Pisces",
-        svg_symbol: "https://en.wikipedia.org/wiki/File:Pisces.svg",
-        unicode_symbol: "♓",
-        gloss: "The Fish",
-        element: "Water",
-        ruling_body_classic: "Jupiter",
-        ruling_body_modern: "Neptune",
-        keywords: ["artistic", "sentimental", "dependent"]
+        image: "/images/pisces.jpg",
+        month: "February 19 to March 20",
+        description: "Pisces is symbolized by two fish swimming in opposite directions. A part of you is always trying to “swim away” or escape reality. The other part is so intuitive that you get swept up in everything going on. Your imagination is the perfect hideout when you want to escape, since Pisces is so creative. You love dancing (Pisces rules the feet), movies, poetry, and music. Your moods are mysterious and your dreams are intense. Every Pisces should have a journal by your bed, since some your best ideas will come in your sleep. Although you can feel helpless, you’re much stronger than you think. The best way for Pisces to experience your own strength is to help people in need. You have great compassion and incredible healing powers. Because it’s easy for people to make you feel guilty, Pisces should watch out for friends who use you, or make you doubt yourself. Surround yourself with good-hearted friends who have their acts together. Many people born under your sign have a “tortured soul” quality. Finding a tranquil spiritual outlet, or spending time alone, can help you get re-centered in your own power."
     }
-];
+    ]
+    
