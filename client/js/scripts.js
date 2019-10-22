6b1ada34be63ce8b1bc91d1a0bcd2708dc273d83
vgusain <vgusain@VGUSAIN-IN.al.oracle.com>
2019-10-23 02:35:00 +0530

    few changes made

---
+++ client/js/scripts.js
@@ -0,0 +1,122 @@
+var zodiacs = [
+    {
+        name : "Aries",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Aries.svg",
+        unicode_symbol : "♈",
+        gloss : "The Ram",
+        element : "Fire",
+        ruling_body_classic : "Mars",
+        ruling_body_modern: "Mars",
+        "keywords": ["driven", "idealistic", "aggressive"]
+    },
+    {
+        name : "Taurus",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Taurus.svg",
+        unicode_symbol: "♉",
+        gloss: "The Bull",
+        element: "Earth",
+        ruling_body_classic: "Venus",
+        ruling_body_modern: "Earth",
+        keywords: ["resolute", "sensual", "indulgent"]
+    },
+    {
+        name: "Gemini",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Gemini.svg",
+        unicode_symbol: "♊",
+        gloss: "The Twins",
+        element: "Air",
+        ruling_body_classic: "Mercury",
+        ruling_body_modern: "Mercury",
+        keywords: ["playful", "curious", "two-faced"]
+    },
+    {
+        name: "Cancer",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Cancer.svg",
+        unicode_symbol: "♋",
+        gloss: "The Crab",
+        element: "Water",
+        ruling_body_classic: "Moon",
+        ruling_body_modern: "Moon",
+        keywords: ["nurturing", "snobby", "petty"]
+    },
+    {
+        name: "Leo",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Leo.svg",
+        unicode_symbol: "♌",
+        gloss: "The Lion",
+        element: "Fire",
+        ruling_body_classic: "Sun",
+        ruling_body_modern: "Sun",
+        keywords: ["fierce", "opulent", "narcissistic"]
+    },
+    {
+        name: "Virgo",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Virgo.svg",
+        unicode_symbol: "♍",
+        gloss: "The Maiden",
+        element: "Earth",
+        ruling_body_classic: "Mercury",
+        ruling_body_modern: "Ceres",
+        keywords: ["meticulous", "helpful", "naive"]
+    },
+    {
+        name: "Libra",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Libra.svg",
+        unicode_symbol: "♎",
+        gloss: "The Scales",
+        element: "Air",
+        ruling_body_classic: "Venus",
+        ruling_body_modern: "Venus",
+        keywords: ["balanced", "chatty", "indecisive"]
+    },
+    {
+        name: "Scorpio",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Scorpio.svg",
+        unicode_symbol: "♏",
+        gloss: "The Scorpion",
+        element: "Water",
+        ruling_body_classic: "Mars",
+        ruling_body_modern: "Pluto",
+        keywords: ["deep", "magnetic", "destructive"]
+    },
+    {
+        name: "Sagittarius",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Sagittarius.svg",
+        unicode_symbol: "♐",
+        gloss: "The Archer",
+        element: "Fire",
+        ruling_body_classic: "Jupiter",
+        ruling_body_modern: "Jupiter",
+        keywords: ["ambitious", "daring", "coarse"]
+    },
+    {
+        name: "Capricon",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Capricorn.svg",
+        unicode_symbol: "♑",
+        gloss: "The Mountain Sea-goat",
+        element: "Earth",
+        ruling_body_classic: "Saturn",
+        ruling_body_modern: "Saturn",
+        keywords: ["responsible", "awkward", "rigid"]
+    },
+    {
+        name: "Aquarius",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Aquarius.svg",
+        unicode_symbol: "♒",
+        gloss: "The Water-bearer",
+        element: "Air",
+        ruling_body_classic: "Saturn",
+        ruling_body_modern: "Uranus",
+        keywords: ["inventive", "zany", "erratic"]
+    },
+    {
+        name: "Pisces",
+        svg_symbol: "https://en.wikipedia.org/wiki/File:Pisces.svg",
+        unicode_symbol: "♓",
+        gloss: "The Fish",
+        element: "Water",
+        ruling_body_classic: "Jupiter",
+        ruling_body_modern: "Neptune",
+        keywords: ["artistic", "sentimental", "dependent"]
+    }
+];
